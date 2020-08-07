87bc3f787ad9f3531cade4a7b1df24bf0ce9c45e
Bob0911 <188140725@qq.com>
2020-08-07 17:53:47 +0800

    update

---
+++ src/Services/Policy/Policy.API/Enum/PolicyStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Policy.API.Enum
+{
+    public enum PolicyStatus
+    {
+        Created = 0,
+        Confrimed = 1,
+        AuditSuccess=2,
+        AuditFail=-2,
+        CustomerPayed=3,
+
+    }
+}
---
+++ src/Services/Policy/Policy.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Policy.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
